99977613e1318b8f03a2ddecdc082b11ba990847
Daniel <dbullock377@comcast.net>
2020-10-04 22:27:23 -0500

    naive

--- naive_bayes.py
+++ naive_bayes.py
@@ -13,6 +13,10 @@ within this file and the last two arguments of line 34 in mp3.py
 and if you want-- the unrevised staff files will be used for all other
 files and classes when code is run, so be careful to not modify anything else.
 """
+from collections import Counter
+
+import numpy as np
+
 
 def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_prior=0.8):
     """
@@ -35,19 +39,49 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
 
     predictions = []
 
-    num_pos = train_labels.count("1")
-    num_neg = train_labels.count("0")
+    posFreq = Counter()  # https://www.geeksforgeeks.org/python-count-occurrences-element-list/
+    negFreq = Counter()  # https://docs.python.org/2/library/collections.html
 
-    print(num_pos)
-    print(num_neg)
+    num_pos = 0
+    num_neg = 0
+    l = len(train_labels)
+    for i in range(l):
+        if train_labels[i] == 1:
+            # num_pos += 1
+            posFreq.update(train_set[i])
+        else:
+            # num_neg += 1
+            negFreq.update(train_set[i])
 
-    print(train_labels.size())
+    pTot = sum(posFreq.values())  # sums all counts of all words up, so as to count all positive words
+    individualPos = len(list(posFreq))
+    nTot = sum(negFreq.values())
+    individualNeg = len(list(negFreq))
 
-    return []
+    priorPosDefault = np.log(pos_prior)
+    priorNegDefault = np.log(1 - pos_prior)
+
+    for review in dev_set:
+        priorPos = priorPosDefault
+        priorNeg = priorNegDefault
+        for word in review:
+            if posFreq[word] == 0:
+                num_pos += 1
+                num_neg += 1
+            temp_p = ((posFreq[word] + smoothing_parameter) / (smoothing_parameter * individualPos + pTot))
+            temp_n = ((negFreq[word] + smoothing_parameter) / (smoothing_parameter * individualNeg + nTot))
+            priorPos += np.log(temp_p)
+            priorNeg += np.log(temp_n)
+        if priorPos > priorNeg:
+            predictions.append(1)
+        else:
+            predictions.append(0)
+
+    return predictions
 
 
-
-def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.0, bigram_smoothing_parameter=1.0, bigram_lambda=0.5,pos_prior=0.8):
+def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.0, bigram_smoothing_parameter=1.0,
+                bigram_lambda=0.5, pos_prior=0.8):
     """
     train_set - List of list of words corresponding with each movie review
     example: suppose I had two reviews 'like this movie' and 'i fall asleep' in my training set
@@ -67,10 +101,5 @@ def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.
     """
     # TODO: Write your code here
     # return predicted labels of development set using a bigram model
-
-
-
-
-
 
     return []
