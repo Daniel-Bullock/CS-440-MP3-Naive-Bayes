49c467c2c007b1efdacab67c6d527fb1a2e5bd8f
Daniel <dbullock377@comcast.net>
2020-10-07 00:38:12 -0500

    b

--- naive_bayes.py
+++ naive_bayes.py
@@ -52,12 +52,14 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
         else:
             # num_neg += 1
             negFreq.update(train_set[i])
-
+    # print(posFreq)
     pTot = sum(posFreq.values())  # sums all counts of all words up, so as to count all positive words
-    individualPos = len(list(posFreq))
+    individualPos = len(list(posFreq))  # total amount of different words that show up as positive
     nTot = sum(negFreq.values())
     individualNeg = len(list(negFreq))
-
+    # print(pTot)
+    # print(individualPos)
+    # print(posFreq["the"])   -- total appearances of an individual word such that it's positive
     priorPosDefault = np.log(pos_prior)
     priorNegDefault = np.log(1 - pos_prior)
 
@@ -101,5 +103,46 @@ def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.
     """
     # TODO: Write your code here
     # return predicted labels of development set using a bigram model
+    predictions = []
 
-    return []
+    posFreq = Counter()  # https://www.geeksforgeeks.org/python-count-occurrences-element-list/
+    negFreq = Counter()  # https://docs.python.org/2/library/collections.html
+
+    num_pos = 0
+    num_neg = 0
+    l = len(train_labels)
+    for i in range(l):
+        if train_labels[i] == 1:
+            # num_pos += 1
+            posFreq.update(train_set[i])
+        else:
+            # num_neg += 1
+            negFreq.update(train_set[i])
+    # print(posFreq)
+    pTot = sum(posFreq.values())  # sums all counts of all words up, so as to count all positive words
+    individualPos = len(list(posFreq))  # total amount of different words that show up as positive
+    nTot = sum(negFreq.values())
+    individualNeg = len(list(negFreq))
+    # print(pTot)
+    # print(individualPos)
+    # print(posFreq["the"])   -- total appearances of an individual word such that it's positive
+    priorPosDefault = np.log(pos_prior)
+    priorNegDefault = np.log(1 - pos_prior)
+
+    for review in dev_set:
+        priorPos = priorPosDefault
+        priorNeg = priorNegDefault
+        for word in review:
+            if posFreq[word] == 0:
+                num_pos += 1
+                num_neg += 1
+            temp_p = ((posFreq[word] + unigram_smoothing_parameter) / (unigram_smoothing_parameter * individualPos + pTot))
+            temp_n = ((negFreq[word] + unigram_smoothing_parameter) / (unigram_smoothing_parameter * individualNeg + nTot))
+            priorPos += np.log(temp_p)
+            priorNeg += np.log(temp_n)
+        if priorPos > priorNeg:
+            predictions.append(1)
+        else:
+            predictions.append(0)
+
+    return predictions
