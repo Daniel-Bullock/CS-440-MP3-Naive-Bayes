aa1ace00ff86b5659e6566910aec929d95b30c68
Daniel <dbullock377@comcast.net>
2020-10-08 16:51:52 -0500

    bi2

--- naive_bayes.py
+++ naive_bayes.py
@@ -13,6 +13,7 @@ within this file and the last two arguments of line 34 in mp3.py
 and if you want-- the unrevised staff files will be used for all other
 files and classes when code is run, so be careful to not modify anything else.
 """
+import math
 from collections import Counter
 
 import numpy as np
@@ -36,7 +37,7 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
     """
     # TODO: Write your code here
     # return predicted labels of development set
-
+    '''
     predictions = []
 
     posFreq = Counter()  # https://www.geeksforgeeks.org/python-count-occurrences-element-list/
@@ -80,19 +81,18 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
             predictions.append(0)
 
     return predictions
-
     '''
+
     unigram_smoothing_parameter = 1.0
     bigram_smoothing_parameter = 1.0
     bigram_lambda = 0.01
     pos_prior = 0.8
     return bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter, bigram_smoothing_parameter,
                        bigram_lambda, pos_prior)
-    '''
 
 
 def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.0, bigram_smoothing_parameter=1.0,
-                bigram_lambda=0.01, pos_prior=0.8):
+                bigram_lambda=0.00001, pos_prior=0.8):
     """
     train_set - List of list of words corresponding with each movie review
     example: suppose I had two reviews 'like this movie' and 'i fall asleep' in my training set
@@ -112,6 +112,7 @@ def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.
     """
     # TODO: Write your code here
     # return predicted labels of development set using a bigram model
+
     bad_predictions = []
     trainingPairs = pairUp(train_set)
     devPairs = pairUp(dev_set)
@@ -124,54 +125,9 @@ def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.
 
         comboPos = (1 - bigram_lambda) * uni[x][0] + bigram_lambda * bi[x][0]
         comboNeg = (1 - bigram_lambda) * uni[x][1] + bigram_lambda * bi[x][1]
-        if comboPos > comboNeg:
-            bad_predictions.append(comboPos)
-        else:
-            bad_predictions.append(comboNeg)
+        bad_predictions.append(int(comboPos > comboNeg))
 
-    predictions = []
-
-    posFreq = Counter()  # https://www.geeksforgeeks.org/python-count-occurrences-element-list/
-    negFreq = Counter()  # https://docs.python.org/2/library/collections.html
-
-    num_pos = 0
-    num_neg = 0
-    l = len(train_labels)
-    for i in range(l):
-        if train_labels[i] == 1:
-            # num_pos += 1
-            posFreq.update(train_set[i])
-        else:
-            # num_neg += 1
-            negFreq.update(train_set[i])
-    # print(posFreq)
-    pTot = sum(posFreq.values())  # sums all counts of all words up, so as to count all positive words
-    individualPos = len(list(posFreq))  # total amount of different words that show up as positive
-    nTot = sum(negFreq.values())
-    individualNeg = len(list(negFreq))
-    # print(pTot)
-    # print(individualPos)
-    # print(posFreq["the"])   -- total appearances of an individual word such that it's positive
-    priorPosDefault = np.log(pos_prior)
-    priorNegDefault = np.log(1 - pos_prior)
-
-    for review in dev_set:
-        priorPos = priorPosDefault
-        priorNeg = priorNegDefault
-        for word in review:
-            if posFreq[word] == 0:
-                num_pos += 1
-                num_neg += 1
-            temp_p = ((posFreq[word] + bigram_smoothing_parameter) / (bigram_smoothing_parameter * individualPos + pTot))
-            temp_n = ((negFreq[word] + bigram_smoothing_parameter) / (bigram_smoothing_parameter * individualNeg + nTot))
-            priorPos += np.log(temp_p)
-            priorNeg += np.log(temp_n)
-        if priorPos > priorNeg:
-            predictions.append(1)
-        else:
-            predictions.append(0)
-
-    return predictions
+    return bad_predictions
 
 
 
@@ -223,8 +179,8 @@ def bigramHelper(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_
     # print(posFreq)
     pTot = sum(posFreq.values())  # sums all counts of all words up, so as to count all positive words
     individualPos = len(list(posFreq))  # total amount of different words that show up as positive
-    print(pTot)
-    print(individualPos)
+    #print(pTot)
+    #print(individualPos)
     nTot = sum(negFreq.values())
     individualNeg = len(list(negFreq))
     # print(pTot)
@@ -248,3 +204,4 @@ def bigramHelper(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_
         predictions.append((priorPos, priorNeg))
     #print(predictions)
     return predictions
+
